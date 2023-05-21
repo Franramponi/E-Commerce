41ecd6b7e4afbe40c0aa160ea170284a4608b827
[Mayra] <[mayrayaz03@gmail.com]>
2023-05-21 19:57:07 -0300

    carousel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <script>
+import {} from "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js";
 import { IonPage } from '@ionic/vue'
+
 export default {
   components: { IonPage }
 }
@@ -9,8 +11,39 @@ export default {
 <template>
   <ion-page>
     <div>
-      <h2 class="title">ME ENCANTAN LOS LIBROS</h2>
-      <P>Y LAS LAPTOPS</P>
+      <h2 class="title">ME ENCANTAN LOS LIBROS Y LAS LAPTOPS</h2>
     </div>
-  </ion-page>
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="src/images/carousel/oferta_encic_pokemon.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="src/images/carousel/oferta_gamer.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="src/images/carousel/oferta_hp.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Anterior</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Siguiente</span>
+  </button>
+ </div>
+
+</ion-page>
 </template>
+
+<style >
+
+</style>
