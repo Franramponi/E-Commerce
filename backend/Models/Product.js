c73bbc6d003a4a98697074470e698449ae3e92d7
Guido Naselli <89815219+guidonaselli@users.noreply.github.com>
2023-06-05 23:58:31 -0300

    Corrección código en User.js
    Falta vincular lo realizado con la base de datos.

--- backend/Models/Product.js
+++ backend/Models/Product.js
@@ -1,4 +1,4 @@
-import {Sequelize, DataTypes} from "sequelize";
+import {DataTypes} from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
--- backend/Models/User.js
+++ backend/Models/User.js
@@ -1,16 +1,7 @@
-import {Sequelize, DataTypes} from "sequelize";
+import {DataTypes} from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-const sequelize = new Sequelize({
-  dialect: 'mssql',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
 
 const User = sequelize.define('User', {
   id: {
