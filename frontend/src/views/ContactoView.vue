05be480f485ff369b02ebb0f3e25433bad4f1100
Guido Naselli <89815219+guidonaselli@users.noreply.github.com>
2023-06-15 22:50:26 -0300

    update del front

---
+++ frontend/src/views/ContactoView.vue
@@ -0,0 +1,48 @@
+<script>
+import { IonPage } from "@ionic/vue";
+import Footer from "./Footer.vue";
+
+export default {
+  components: { Footer, IonPage },
+};
+</script>
+<template>
+  <ion-page>
+    <ion-content>
+      <div class="page-body">
+        <div class="contacto">
+          <div class="map">
+            <h2>Ubicación</h2>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.611352560773!2d-58.400708425428334!3d-34.58869929692481!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca988d30e3e1%3A0x13939a1f21db3a6c!2sAzcu%C3%A9naga%201800%2C%20C1126%20CABA!5e0!3m2!1ses-419!2sar!4v1686263637486!5m2!1ses-419!2sar"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+          <section class="form-contacto">
+            <h2>Contacto</h2>
+           <!--  <div class="form"> -->
+              <form action="/enviar" method="post">
+                <label for="nombre">Nombre</label>
+                <input type="text" id="nombre" />
+
+                <label for="email">Correo electrónico</label>
+                <input type="email" id="email" required />
+
+                <label for="telefono">Telefono</label>
+                <input type="numer" id="telefono" />
+
+                <label for="mensaje">Mensaje</label>
+                <textarea id="mensaje"></textarea>
+
+              <button type="submit">Enviar</button>
+              </form>
+            </section>
+        </div>
+        <Footer />
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
---
+++ frontend/src/views/ProductsView.vue
@@ -0,0 +1,125 @@
+<script>
+import { IonPage, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonImg, IonButton, IonInput } from '@ionic/vue'
+//import json from '../data/products.json'
+import productService from '../services/productService.js'
+import { useCartStore } from '../stores/cart.js'
+import { storeToRefs } from 'pinia';
+import { useLoginStore } from '../stores/login.js'
+import Footer from './Footer.vue'
+
+export default {
+  components: { Footer, IonPage, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonImg, IonButton, IonInput },
+  setup() {
+		const store = useLoginStore();
+		const { isLogin } = storeToRefs(store);
+		return { isLogin };
+	},
+	data() {
+		return {
+			//products: json
+      products: [],
+      filters: [
+        {
+          id: 1,
+          label: "Filtro Numero 1",
+          defaultValue: 0,
+          type: "number"
+        },
+        {
+          id: 2,
+          label: "Filtro Numero 2",
+          defaultValue: 2,
+          type: "number"
+        },
+        {
+          id: 3,
+          label: "Filtro Numero 3",
+          defaultValue: 3,
+          type: "number"
+        },
+        {
+          id: 4,
+          label: "Filtro Password 1",
+          defaultValue: "hi",
+          type: "password"
+        }
+      ]
+		}
+	},
+  mounted() {
+    //Esto corre cuando la pagina abre
+    this.loadData();
+    this.filters.forEach(e => {
+      console.log(e.defaultValue);
+      e.value = e.defaultValue;
+    });
+  },
+  methods: {
+    errorCatch(err) {
+      console.log(err);
+      alert(err);
+    },
+    loadData() {
+      //Nuestra DB:
+      productService.loadData((res) => { this.products = res.data.products; }, this.errorCatch);
+
+      //mockapi:
+      //productService.loadData((res) => { this.products = res.data; }, this.errorCatch);
+    },
+    addToCart(id) {
+      console.log(this.filters);
+      if(this.isLogin){
+        //Nuestra DB:
+        productService.getData(id, (res) => { console.log(res.data); useCartStore().addProduct({...res.data.product}); }, this.errorCatch);
+        
+        //mockapi:
+        //productService.getData(id, (res) => { console.log(res.data); useCartStore().addProduct({...res.data}); }, this.errorCatch);
+      
+      } else {
+        this.$router.push("/login");
+      }
+
+    }
+    /*
+    getImg(image) {
+      return new URL('../images/' + image, import.meta.url).href;
+    }
+    */
+  }
+}
+</script>
+
+<template>
+  <ion-page>
+    <ion-content>
+      <div class="page-body">
+        <h2 class="title">Products page</h2>
+        <div class=products-filters-split>
+          <div class="filters-column">
+            <div class="filter" v-for="f in filters" :key="f.id">
+              <label class="filter-label" :for="'filter' + f.id">{{ f.label }}:</label>
+              <ion-input class="filter-input" :name="'filter' + f.id" :value="f.defaultValue" :type="f.type" v-model="f.value"></ion-input>
+            </div>
+          </div>
+          <div class="products-column invisible-scroll">
+            <div class="products-view">
+              <ion-card class="product-card" v-for="p in products" :key="p.id">
+                <ion-img class="products-img" :src=p.image></ion-img>
+                <!-- <ion-img :src=getImg(p.image)></ion-img> -->
+                
+                <ion-card-header>
+                  <ion-card-title class="products-name">{{ p.name }}</ion-card-title>
+                </ion-card-header>
+                <ion-card-content class="products-desc">{{ p.description }}</ion-card-content>
+                <ion-card-subtitle class="products-price">${{ p.price }}</ion-card-subtitle>
+                <ion-button class="products-btn" @click="addToCart(p.id)"><font-awesome-icon icon="fa-solid fa-shopping-cart" class="cart-icon"/></ion-button>
+              </ion-card>
+            </div>
+          </div>
+        </div>
+        
+        <Footer/>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
